cc43166a7a813269ea4749032213cb4d35aec828
Thiago <th0608thiago@gmail.com>
2024-04-20 17:49:23 -0300

    Text Menu

---
+++ MountainShootar/pythonProject1/code/Enemy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Entyti import Entyti
+
+class Enemy(Entyti):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
+
---
+++ MountainShootar/pythonProject1/code/Player.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Entyti import Entyti
+
+class Player(Entyti):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
+
